5c1b3de0befbf1d93b0334a935c700edbe48fe21
aakashsarang <beastgaming0004@gmail.com>
2025-07-05 12:57:06 +0530

    Create bubblesortde.cpp

---
+++ bubblesortde.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void bubblesort(int arr[],int n){
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    swap(arr[j],arr[j+1]);
+                }
+        }
+    }
+}
+void printarr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int n=5;
+    int arr[]={4,1,2,8,9};
+    bubblesort(arr,n);
+    printarr(arr,n);
+    return 0;
+}
